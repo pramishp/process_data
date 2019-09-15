0a55ff6f81e5acbda73faef33834a604f580f10c
pramishp <pramish.paudel123@gmail.com>
2019-09-15 17:59:37 +0545

    packages added in setup

--- process_data_vicaaya/__init__.py
+++ process_data_vicaaya/__init__.py
@@ -1,8 +1,1 @@
 name = "process_data_vicaaya"
-# from process_data_vicaaya.Hosts import *
-# from process_data_vicaaya.Stream import *
-# from process_data_vicaaya.utils import *
-# from process_data_vicaaya.constants import *
-#
-
-
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from distutils.core import setup
 setup(
     name='process_data_vicaaya',
     version='0.0.1',
+    packages=['process_data_vicaaya', 'process_data_vicaaya.Stream'],
     license='Apache License, Version 2.0',
     description='Post processing for one of my project',
     author='Pramish Paudel',
